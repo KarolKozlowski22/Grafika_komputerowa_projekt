e86724d327ddf075718f1d064121ceb4d0d6f3d1
Aleksander Sieklinski <aleksandersieklinski@gmail.com>
2023-04-22 18:50:32 +0200

    C

--- main.cpp
+++ main.cpp
@@ -1,14 +1,1 @@
 #include <iostream> 
-
-
-
-int main() {
-
-    std::cout << "Hello World!" << std::endl;
-    //std::cout << "Hello World!" << std::endl;
-
-    return 0;
-
-}   
-#include <iostream>
-#include <string>
